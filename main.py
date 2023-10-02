3a1ef29046ac151a0c34f73f412f7c3d9e543e25
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-01 23:15:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-def subtraction(x,y):
-    result = 0
-    result = x - y
-    return result
+"""
+ETL-Query script
+"""
+import sys
+from mylib.extract import extract
+from mylib.transform_load import load
+from mylib.query import query
+
+def main():
+    try:
+        # Extract
+        print("Extracting data...")
+        file_path = extract() 
+        print(f"Data extraction completed successfully. Saved to {file_path}\n")
+
+        # Transform and Load
+        print("Transforming and loading data...")                         
+        load(file_path)                
+        print("Data transformation and loading completed successfully.\n")
+
+        # Query
+        print("Querying data...")
+        query()
+        print("Data querying completed successfully.\n")
+
+    except Exception as e:
+        print(f"An error occurred: {e}", file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
